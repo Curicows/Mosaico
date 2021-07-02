4e549065ca6c94496ec22a12de33abad5611376d
Giovane Marcelo <giovane0845@gmail.com>
2021-07-02 00:21:39 -0300

    - Fullscreen - Mosaico class - DialogBrowser - KeyPress test

---
+++ Mosaico/Mosaico.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mosaico
+{
+    public class Mosaico
+    {
+        private Image[] imagens;
+        private Timer timer = new Timer();
+        private FolderBrowserDialog browser = new FolderBrowserDialog();
+        
+        private int intervalo = 5000;
+        
+        public Mosaico()
+        {
+            
+        }
+
+        public string browseFiles()
+        {
+            this.browser.ShowDialog();
+            return this.browser.SelectedPath;
+        }
+
+        
+        
+        
+        
+        
+        
+        
+        public void StartTimer()
+        {
+            this.timer.Start();
+        }
+
+        public void StopTimer()
+        {
+            this.timer.Stop();
+        }
+
+        public void setIntervalo(int intervalo)
+        {
+            this.intervalo = intervalo;
+            this.timer.Interval = intervalo;
+        }
+
+        public int getIntervalo()
+        {
+            return this.intervalo;
+        }
+
+
+    }
+}
